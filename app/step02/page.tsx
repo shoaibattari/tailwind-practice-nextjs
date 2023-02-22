ba9f8e86d7feac7b32b8f8798aeb6305b67084ba
shoaib memon <shoaibattari83@gmail.com>
2023-02-23 00:20:01 +0500

    check components tailwind css

--- app/step02/page.tsx
+++ app/step02/page.tsx
@@ -9,10 +9,10 @@ export default function page() {
       <div className="mx-6 order-last my-8">
         <Image src={panaversePic} width={180} height={80} alt="abc" />
       </div>
-      <div className=" mx-7 self-center">
+      <div className=" mx-7 self-center text-center">
         <h1 className="text-blue-700 text-6xl font-bold">Wellcome To Panaverse</h1>
         <h2 className="text-blue-400 text-4xl font-bold">A Community Of Web3 And Metaverse Developer</h2>
-        <button className=" my-4 px-4 py-2 border-2 rounded-lg text-white bg-blue-900" >Learn More</button>
+        <button className=" my-4 px-8  py-2 border-2 rounded-lg text-white bg-blue-900 " >Learn More</button>
       </div>
     </div>
   );
